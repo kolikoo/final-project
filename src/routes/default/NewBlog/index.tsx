b0639c899952d50cdd9a32350b3da5fb007b16f7
kolikoo <nikushasulxanishvili@gmail.com>
2025-01-27 01:31:17 +0400

    finally

--- src/routes/default/NewBlog/index.tsx
+++ src/routes/default/NewBlog/index.tsx
@@ -4,8 +4,6 @@ import { Route } from "react-router-dom";
 import { Default_Layout_Path } from "../index.enum";
 import NewBlogs from "@/Pages/Clothes-blogs/new/New-blog";
 
-
-
 export const New_Blogs = [
   <Route
     path={Default_Layout_Path.NewBlog}
